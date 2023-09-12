a60bdec7eacdb60446ae01e9b96f58d5ac361627
Dave Branson <david@thebransons.us>
2023-09-11 18:50:11 -0700

    Added comments

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Two Line Intersection from Endpoints
  Calculating the intersection of two lines given the endpoint coordinates
+
+ 
--- line_intercept.py
+++ line_intercept.py
@@ -1,5 +1,8 @@
-
+# Import modules
 import matplotlib.pyplot as plt
+
+
+# Set coordinates of both lines' endpoints
 xa = 2
 ya = 3
 xb = 8
@@ -25,18 +28,26 @@ if xb-xa == 0.0:
 if xc == xd:
   xd += 0.000001
 
+
+# Calculate slopes of both lines
 m1 = round((yb-ya)/(xb-xa),5)
 m2 = round((yd-yc)/(xd-xc),5)
 print(f"Slope of line AB: {m1}")
-print(f"Slope of line BC: {m2}")
+print(f"Slope of line CD: {m2}")
 
+# Calculate x for intersection
 x = (m1*xa-ya-m2*xc+yc)/(m1-m2)
+
+# Calculate y for intersection
 y = m1*(x-xa)+ya
+
+# Limit decimal places
 x = round(x,5)
 y = round(y,5)
+
+
 print(f"intersection: x={x}, y={y}")
 intersection_label = "intersection"
-plt.xkcd()
 plt.plot([xa,xb],[ya,yb], label = "line AB")
 plt.plot([xc,xd],[yc,yd], label = "line CD")
 plt.plot(x,y, 'o', label = intersection_label,)
